e9f7ab2e073815c2f5ca1edb0abe8bd9a27a1e68
John Okyere <112992966+mhiskall282@users.noreply.github.com>
2026-06-23 18:34:11 +0000

    fix: explicit prisma client output folder for vercel compat

--- api/prisma.ts
+++ api/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../prisma/client/index.js';
 
 // Prevent multiple instances of Prisma Client in development
 declare global {
